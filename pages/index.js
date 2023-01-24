486b8902dc32609ef267f672d96b1c0802ae857a
Raphael <psyruz18@gmail.com>
2023-01-24 14:09:46 +0800

    exp3

--- pages/index.js
+++ pages/index.js
@@ -4,30 +4,49 @@ import Posts from "../components/Posts";
 
 import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "../lib/firebase";
+import { useEffect, useState } from "react";
 
-export async function getServerSideProps() {
-  const res = await getDocs(
-    query(collection(db, "posts"), orderBy("dateCreated", "desc"))
-  );
+// export async function getServerSideProps() {
+//   const res = await getDocs(
+//     query(collection(db, "posts"), orderBy("dateCreated", "desc"))
+//   );
 
-  if (!res) {
-    return {
-      notFound: true,
+//   if (!res) {
+//     return {
+//       notFound: true,
+//     };
+//   }
+
+//   const posts = res.docs.map((doc) => {
+//     return { id: doc.id, ...doc.data() };
+//   });
+
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// }
+
+export default function Index() {
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getDocs(
+        query(collection(db, "posts"), orderBy("dateCreated", "desc"))
+      );
+
+      const posts = res.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+
+      setPosts(posts);
     };
-  }
 
-  const posts = res.docs.map((doc) => {
-    return { id: doc.id, ...doc.data() };
-  });
+    fetchData();
+  }, []);
 
-  return {
-    props: {
-      posts,
-    },
-  };
-}
-
-export default function Index({ posts }) {
   return (
     <Box sx={{ backgroundColor: "#EEF0F1" }}>
       <Navbar />
