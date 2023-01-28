2d613dc3f1af7f1617a8ae5a7b16b33bd9dba1f2
Raphael <psyruz18@gmail.com>
2023-01-28 14:32:53 +0800

    client

--- pages/index.js
+++ pages/index.js
@@ -6,35 +6,32 @@ import { collection, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "../lib/firebase";
 import { useEffect, useState } from "react";
 
-export async function getServerSideProps() {
-  const res = await getDocs(
-    query(collection(db, "posts"), orderBy("dateCreated", "desc"))
-  );
+export default function Index() {
+  const [posts, setPosts] = useState("");
 
-  if (!res) {
-    return {
-      notFound: true,
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getDocs(
+        query(collection(db, "posts"), orderBy("dateCreated", "desc"))
+      );
+
+      if (!res) {
+        return {
+          notFound: true,
+        };
+      }
+
+      const fetchedPosts = res.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
+
+      setPosts(fetchedPosts);
     };
-  }
 
-  const posts = res.docs.map((doc) => {
-    return { id: doc.id, ...doc.data() };
-  });
+    fetchData();
+  }, []);
 
-  return {
-    props: {
-      posts,
-    },
-  };
-}
-
-export default function Index({ posts }) {
   return (
-    // <div>
-    //   {posts.map((post) => {
-    //     return <h1>{post.postMessage}</h1>;
-    //   })}
-    // </div>
     <Box sx={{ backgroundColor: "#EEF0F1" }}>
       <Navbar />
       {posts && <Posts posts={posts} />}
